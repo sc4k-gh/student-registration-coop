eb982387d6e33a6ff200d71928676f28eebddace
ColeS13 <colesawchuk3@gmail.com>
2026-03-25 11:30:45 -0400

    Added 2 endpoints, Supabase information is placeholder

---
+++ server/src/routes/locations.js
@@ -0,0 +1,14 @@
+const express = require('express')
+const app = express()
+const { createClient } = require("@supabase/supabase-js");
+const supabaseUrl = 'https://tezxtqtabsfxdavbmgpp.supabase.co';
+const supabaseKey = 'sb_publishable_np5n2nhgYZGBDxq_QZdoUg_p8Ck5NRT';
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+//Get a list of all current locations and associated information
+app.get('/locations', async (req, res) => {
+    const { data, error } = await supabase
+        .from('locations')
+        .select()
+  res.send(data)
+})
